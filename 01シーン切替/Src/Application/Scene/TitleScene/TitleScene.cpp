ba36e404d2d7de92c48b3f892dc95b1ead4deda7
kd1372033 <kd1372033@st.kobedenshi.ac.jp>
2026-04-22 15:06:34 +0900

    シーン切り替え
    前方宣言で呼び出し、タイトル画面制作

---
+++ 01シーン切替/Src/Application/Scene/TitleScene/TitleScene.cpp
@@ -0,0 +1,31 @@
+﻿#include "TitleScene.h"
+
+void TitleScene::Init()
+{
+	m_tex.Load("Asset/Textures/title.png");
+	m_starttex.Load("Asset/Textures/start.png");
+
+	m_alpha = 0.0f;
+	m_delta = 0.01f;
+}
+
+void TitleScene::Update()
+{
+	m_alpha += m_delta;
+	if (m_alpha <= 0.5) { m_delta = 0.01f; }
+	else if (m_alpha >= 1) { m_delta = -0.01f; }
+
+}
+
+void TitleScene::DrawSprite()
+{
+	KdShaderManager::Instance().m_spriteShader.DrawTex(&m_tex, 0, 0);
+	Math::Color color(1, 1, 1, m_alpha);
+	KdShaderManager::Instance().m_spriteShader.DrawTex(&m_starttex, 0, -200, nullptr, &color);
+
+}
+
+void TitleScene::Release()
+{
+
+}
