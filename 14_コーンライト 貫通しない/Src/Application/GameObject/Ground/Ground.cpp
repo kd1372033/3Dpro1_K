3140c8a434efb36a5df8ed2e16c2dc9f40d4e0da
kd1372033 <kd1372033@st.kobedenshi.ac.jp>
2026-09-08 11:26:16 +0900

    コーンライト
    視界表現に適した扇形のコーンライトの表示
    横側にも減衰率を付けてリアルな光に近づけた

---
+++ 14_コーンライト 貫通しない/Src/Application/GameObject/Ground/Ground.cpp
@@ -0,0 +1,27 @@
+﻿#include "Ground.h"
+
+void Ground::Init()
+{
+	//メモリ確保
+	m_model = std::make_shared<KdModelData>();
+	//モデルの読み込み
+	m_model->Load("Asset/Models/Ground/Ground.gltf");
+
+	Math::Matrix scaleMat = Math::Matrix::CreateScale(100.0f);
+	//座標行列
+	Math::Matrix transMat = Math::Matrix::CreateTranslation(0, 0, 0);
+
+	// 行列合成 (S * R * T)
+	m_mWorld = scaleMat * transMat;
+
+}
+
+void Ground::DrawLit()
+{
+	//地面描画
+	KdShaderManager::Instance().m_StandardShader.DrawModel(*m_model, m_mWorld);
+	Math::Matrix rotMat = Math::Matrix::CreateRotationX(DirectX::XMConvertToRadians(90));
+	Math::Matrix scaleMat = Math::Matrix::CreateScale(100.0f);
+	Math::Matrix mat = scaleMat * rotMat;
+	KdShaderManager::Instance().m_StandardShader.DrawModel(*m_model, mat);
+}
